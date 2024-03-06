ab4dcd0afba8c8e1aceac9a21e54db44e9ea5c50
Jonsman <jeans@unitybox.de>
2024-03-07 00:09:07 +0100

    debug mode

--- main.py
+++ main.py
@@ -37,5 +37,5 @@ def token():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    #app.run(host="0.0.0.0", port=5000)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
