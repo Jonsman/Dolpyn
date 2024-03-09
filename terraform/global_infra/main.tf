04d8864bcd890a689e611cbda54e8ac606c9f27b
Jonsman <jeans@unitybox.de>
2024-03-09 23:43:27 +0100

    terraform backend fix

--- terraform/global_infra/main.tf
+++ terraform/global_infra/main.tf
@@ -1,9 +1,9 @@
 # Configuration
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_globalinfra["name"]
-    storage_account_name = var.storage_account_name
-    container_name       = var.storage_container_name
+    resource_group_name  = "rg-globalinfra-prod-euwest-001" # Can't use variables here
+    storage_account_name = "stdolpyndata001"                # Can't use variables here
+    container_name       = "terraform"                      # Can't use variables here
     key                  = "terraform.tfstate"
   }
 
@@ -17,12 +17,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-/*
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  */
 }
 
 # Azure Resources
@@ -52,13 +46,3 @@ resource "azurerm_container_registry" "cr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
-
-/*
-resource "azurerm_key_vault" "kv" {
-  name                = var.key_vault_name
-  resource_group_name = azurerm_resource_group.rg-globalinfra.name
-  location            = azurerm_resource_group.rg-globalinfra.location
-  tenant_id           = var.tenant_id
-  sku_name            = "standard"
-}
-*/
