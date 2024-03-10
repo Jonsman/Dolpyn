f314b1b054f8d32a4aa4ca6406c7da671c0fd456
Jonsman <jeans@unitybox.de>
2024-03-10 03:29:18 +0100

    final fix

--- terraform/container_app/main.tf
+++ terraform/container_app/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_container_app" "ca" {
   template {
     container {
       name   = "dolpyn"
-      image  = var.acr_name + "azurecr.io/dolpyn:latest"
+      image  = "${var.acr_name}.azurecr.io/dolpyn:latest"
       cpu    = 0.25
       memory = "0.5Gi"
     }
